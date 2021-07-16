37c5ac04f4f642d812b2e8ee71740c09c9662ba7
Drambow716 <walid.lakouader@gmail.com>
2021-07-16 11:58:25 +0300

    First Deployment

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import sympy as sp
+import time
+
+st.header("Weclome to the best math derivative in the market")
+st.text("This application was backed by Elon Musk him self :) ")
+
+
+expr = st.text_input("Please input your formula", "((x**2)/2)-5*x+1")
+
+x = sp.symbols("x")
+
+try:
+    Answer = sp.diff(expr)
+    my_bar = st.progress(0)
+
+    for something in range(100):
+        time.sleep(0.1)
+        my_bar += st.progress(0)
+    Answer
+except:
+    pass
+
+
